bc5db6039fdbb2c77abc711304b3f17abdb199b0
Yagya Chaudhary <mail@bdryagya.com.np>
2021-08-09 15:48:20 +0545

    support more gates

--- src/gates/index.ts
+++ src/gates/index.ts
@@ -3,4 +3,8 @@ import { bit } from '../types';
 export const or = (a: bit, b: bit): bit => (a | b) as bit;
 export const and = (a: bit, b: bit): bit => (a & b) as bit;
 export const not = (a: bit): bit => (~a & 1) as bit;
+
 export const xor = (a: bit, b: bit): bit => (a ^ b) as bit;
+export const nor = (a: bit, b: bit): bit => not(or(a, b));
+export const nand = (a: bit, b: bit): bit => not(and(a, b));
+export const xnor = (a: bit, b: bit): bit => not(xor(a, b));

985c15c7c05bc8b5a6c9d82bed0c21e58375c0b0
Yagya Chaudhary <mail@bdryagya.com.np>
2021-08-10 14:20:07 +0545

    remove unused types

--- src/types.ts
+++ src/types.ts
@@ -3,29 +3,3 @@ export type bit = 0 | 1;
 export type io = {
   v: bit;
 };
-
-export type sr_io = {
-  s: io;
-  r: io;
-  q: io;
-  q_: io;
-};
-
-export type addr_io = {
-  a: io;
-  b: io;
-  ci: io;
-  s: io;
-  co: io;
-};
-
-export type u_io = {
-  i: io;
-  o: io;
-};
-
-export type b_io = {
-  a: io;
-  b: io;
-  o: io;
-};

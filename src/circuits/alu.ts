911dd9508f9d3d9a4ca8f2690d77df90f125dee9
Yagya Chaudhary <mail@bdryagya.com.np>
2021-08-18 22:35:21 +0545

    flags

--- src/circuits/alu.ts
+++ src/circuits/alu.ts
@@ -55,6 +55,9 @@ export const alu = (co: io, r: io[], a: io[], b: io[], f: io[], op: io[]) => {
    * 11: AND
    */
 
+  const _op: io[] = [{ v: 0 }, { v: 0 }, { v: 0 }, { v: 0 }];
+  _dec(_op, op);
+
   // ADD
   const _sum_co: io = { v: 0 };
   addr(_sum_co, _rsum, a, b);
@@ -83,20 +86,22 @@ export const alu = (co: io, r: io[], a: io[], b: io[], f: io[], op: io[]) => {
   or(_ror[1], a[1], b[1]);
   or(_ror[0], a[0], b[0]);
 
+  // result
+  _4mux(r[7], [_rand[7], _ror[7], _rsub[7], _rsum[7]], op);
+  _4mux(r[6], [_rand[6], _ror[6], _rsub[6], _rsum[6]], op);
+  _4mux(r[5], [_rand[5], _ror[5], _rsub[5], _rsum[5]], op);
+  _4mux(r[4], [_rand[4], _ror[4], _rsub[4], _rsum[4]], op);
+  _4mux(r[3], [_rand[3], _ror[3], _rsub[3], _rsum[3]], op);
+  _4mux(r[2], [_rand[2], _ror[2], _rsub[2], _rsum[2]], op);
+  _4mux(r[1], [_rand[1], _ror[1], _rsub[1], _rsum[1]], op);
+  _4mux(r[0], [_rand[0], _ror[0], _rsub[0], _rsum[0]], op);
+
   // carry flag
-  _2mux(co, [_sub_co, _sum_co], op[0]);
+  _4mux(co, [{ v: 0 }, { v: 0 }, _sub_co, _sum_co], _op);
   f[0] = co;
 
   // zero flag
   const _z: io = { v: 0 };
-  or(_z, _rsum[7], _rsum[6]);
-  or(_z, _z, _rsum[5]);
-  or(_z, _z, _rsum[4]);
-  or(_z, _z, _rsum[3]);
-  or(_z, _z, _rsum[3]);
-  or(_z, _z, _rsum[2]);
-  or(_z, _z, _rsum[1]);
-  or(_z, _z, _rsum[0]);
   or(_z, _z, _rsub[7]);
   or(_z, _z, _rsub[6]);
   or(_z, _z, _rsub[5]);
@@ -108,13 +113,6 @@ export const alu = (co: io, r: io[], a: io[], b: io[], f: io[], op: io[]) => {
 
   not(f[1], _z);
 
-  // result
-  _4mux(r[7], [_rand[7], _ror[7], _rsub[7], _rsum[7]], op);
-  _4mux(r[6], [_rand[6], _ror[6], _rsub[6], _rsum[6]], op);
-  _4mux(r[5], [_rand[5], _ror[5], _rsub[5], _rsum[5]], op);
-  _4mux(r[4], [_rand[4], _ror[4], _rsub[4], _rsum[4]], op);
-  _4mux(r[3], [_rand[3], _ror[3], _rsub[3], _rsum[3]], op);
-  _4mux(r[2], [_rand[2], _ror[2], _rsub[2], _rsum[2]], op);
-  _4mux(r[1], [_rand[1], _ror[1], _rsub[1], _rsum[1]], op);
-  _4mux(r[0], [_rand[0], _ror[0], _rsub[0], _rsum[0]], op);
+  // negative flag, OP is SUB and MSB is 1
+  and(f[2], r[0], _op[2]);
 };
--- test/alu.test.ts
+++ test/alu.test.ts
@@ -6,7 +6,7 @@ import { _io } from './helpers';
 describe('ALU', () => {
   test('sum', () => {
     const co: io = { v: 0 };
-    const f: io[] = _io([0, 0, 0]);
+    const f: io[] = _io([0, 0, 0, 0]);
     const r: io[] = _io([0, 0, 0, 0, 0, 0, 0, 0]);
 
     alu(co, r, __8decoder(123), __8decoder(111), f, _io([0, 0]));
@@ -20,7 +20,7 @@ describe('ALU', () => {
 
   test('sub', () => {
     const co: io = { v: 0 };
-    const f: io[] = _io([0, 0, 0]);
+    const f: io[] = _io([0, 0, 0, 0]);
     const r: io[] = _io([0, 0, 0, 0, 0, 0, 0, 0]);
 
     alu(co, r, __8decoder(123), __8decoder(111), f, _io([0, 1]));
@@ -34,7 +34,7 @@ describe('ALU', () => {
 
   test('or', () => {
     const co: io = { v: 0 };
-    const f: io[] = _io([0, 0, 0]);
+    const f: io[] = _io([0, 0, 0, 0]);
     const r: io[] = _io([0, 0, 0, 0, 0, 0, 0, 0]);
 
     alu(co, r, __8decoder(123), __8decoder(111), f, _io([1, 0]));
@@ -46,7 +46,7 @@ describe('ALU', () => {
 
   test('and', () => {
     const co: io = { v: 0 };
-    const f: io[] = _io([0, 0, 0]);
+    const f: io[] = _io([0, 0, 0, 0]);
     const r: io[] = _io([0, 0, 0, 0, 0, 0, 0, 0]);
 
     alu(co, r, __8decoder(123), __8decoder(111), f, _io([1, 1]));
@@ -57,5 +57,36 @@ describe('ALU', () => {
 
     alu(co, r, __8decoder(0), __8decoder(255), f, _io([1, 1]));
     expect(__8encoder(r)).toBe(255 & 0);
+  });
+});
+describe('Flags', () => {
+  test('carry flag', () => {
+    const co: io = { v: 0 };
+    const f: io[] = _io([0, 0, 0, 0]);
+    const r: io[] = _io([0, 0, 0, 0, 0, 0, 0, 0]);
+
+    alu(co, r, __8decoder(255), __8decoder(1), f, _io([0, 0]));
+    expect(f).toEqual(_io([1, 0, 0, 0]));
+  });
+
+  test('zero flag', () => {
+    const co: io = { v: 0 };
+    const f: io[] = _io([0, 0, 0, 0]);
+    const r: io[] = _io([0, 0, 0, 0, 0, 0, 0, 0]);
+
+    alu(co, r, __8decoder(127), __8decoder(127), f, _io([0, 1]));
+    expect(f).toEqual(_io([0, 1, 0, 0]));
+
+    alu(co, r, __8decoder(127), __8decoder(126), f, _io([0, 1]));
+    expect(f).toEqual(_io([0, 0, 0, 0]));
+  });
+
+  test('negative flag', () => {
+    const co: io = { v: 0 };
+    const f: io[] = _io([0, 0, 0, 0]);
+    const r: io[] = _io([0, 0, 0, 0, 0, 0, 0, 0]);
+
+    alu(co, r, __8decoder(12), __8decoder(127), f, _io([0, 1]));
+    expect(f).toEqual(_io([0, 0, 1, 0]));
   });
 });
